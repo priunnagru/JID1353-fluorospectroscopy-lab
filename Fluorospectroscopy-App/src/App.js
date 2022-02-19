e1c1cef8383c2143c1b665c20390b77bc4e46540
priunnagru <priunnagru@gmail.com>
2022-02-18 20:20:11 -0500

    Added the initial screen to start tutorial Added the first slide of the tutorial

--- Fluorospectroscopy-App/src/App.js
+++ Fluorospectroscopy-App/src/App.js
@@ -2,7 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Menu from './screens/Menu'
 import Tutorial from './screens/Tutorial'
-import TutorialNew from './screens/tutorial/TutorialNewA'
+import TutorialNewA from './screens/tutorial/TutorialNewA'
+import TutorialNewB from './screens/tutorial/TutorialNewB'
+import TutorialNewC from './screens/tutorial/TutorialNewC'
 import Lab from './screens/Lab'
 import Settings from './screens/Settings'
 
@@ -11,7 +13,11 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Menu />}/>
-        <Route path="/tutorial" element={<TutorialNew />}/>
+
+        <Route path="/tutorial" element={<TutorialNewA />}/>
+        <Route path="/tutorial/1" element={<TutorialNewB />}/>
+        <Route path="/tutorial/2" element={<TutorialNewC />}/>
+
         <Route path="/lab" element={<Lab />}/>
         <Route path="/settings" element={<Settings />}/>
       </Routes>
--- Fluorospectroscopy-App/src/screens/tutorial/TutorialNewC.js
+++ Fluorospectroscopy-App/src/screens/tutorial/TutorialNewC.js
@@ -1,10 +1,11 @@
 import { Button, Typography, Box, Paper, LinearProgress } from '@mui/material';
 import TechGold from '../../resources/GeorgiaTech_TechGold.png'
+import Tryptophan from '../../resources/Tryptophan.svg'
 import '../../styles/tutorial_styles.css';
 import { Link } from 'react-router-dom';
 import NavigateNext from '@mui/icons-material/NavigateNext';
 
-const TutorialNewA = () => {
+const TutorialNewC = () => {
   return (
     <>
       <header>
@@ -27,26 +28,14 @@ const TutorialNewA = () => {
             </div>
           </div>
 
-          <LinearProgress color="secondary" variant="determinate" value={(100.0/15) * 0} />
+          <LinearProgress color="secondary" variant="determinate" value={(100.0/15) * 2} />
         </Paper>
       </header>
 
       <div className="tutorial-body">
-        <Box display="flex" justifyContent="center" m={2}>
-          <Typography className= "general-text" variant="h2" color="secondary">
-            What is fluoresence?
-          </Typography>
-        </Box>
-
-        <Box display="flex" justifyContent="center" m={2}>
-          <Button variant="contained" color="primary" endIcon={<NavigateNext/>} component={Link} to="/tutorial/1">
-            Begin
-          </Button>
-        </Box>
       </div>
-
     </>
   );
 }
 
-export default TutorialNewA;
+export default TutorialNewC;
